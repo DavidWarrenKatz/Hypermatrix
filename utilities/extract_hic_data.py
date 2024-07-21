46e33a1a7596d09e5a39bf21752582b7362bd82a
chewe.kasonde@outlook.com <chewe.kasonde@outlook.com>
2024-07-21 14:30:46 -0400

    upgraded bash and seperated code logic

---
+++ utilities/extract_hic_data.py
@@ -0,0 +1,52 @@
+#!/bin/env python3
+# extract_hic_data.py
+
+import sys
+import numpy as np
+import hicstraw
+import h5py
+
+def extract_hic_data(path, hic_url, resolutions, chromosomes, data_types):
+    hic = hicstraw.HiCFile(hic_url)
+    normalization = 'KR'
+    resolutions = [int(r.strip()) for r in resolutions.split(",")]
+    chromosomes = [c.strip().strip("'") for c in chromosomes.split(",")]
+    data_types = [d.strip().strip("'") for d in data_types.split(",")]
+
+    for resolution in resolutions:
+        for chromosome in chromosomes:
+            startingbp = 0
+            endingbp = hic.getChromosomes()[int(chromosome)].length
+            for data in data_types:
+                matrix_object = hic.getMatrixZoomData(chromosome, chromosome, data, normalization, "BP", resolution)
+                matrix = matrix_object.getRecordsAsMatrix(startingbp, endingbp, startingbp, endingbp)
+                with open(f'{path}hicFiles/short_score_textform/shortScore_res{resolution}_ch{chromosome}_{data}_{normalization}.txt', 'wt') as fid_org:
+                    for i in range(matrix.shape[0]):
+                        for j in range(i, matrix.shape[1]):
+                            if matrix[i, j] != 0:
+                                fid_org.write('0 {} {} 0 0 {} {} 1 {:.5f}\n'.format(chromosome, startingbp + resolution * i, chromosome, startingbp + resolution * j, matrix[i, j]))
+
+                new_filename = f'{path}Workspaces/individual/ch{chromosome}_res{resolution}_{data}_{normalization}.mat'
+                with h5py.File(new_filename, 'w') as hf:
+                    hf.create_dataset('matrix', data=matrix)
+
+    for data in data_types:
+        with open(f'{path}hicFiles/short_score_textform/shortScore_all_{data}_KR.txt', 'wt') as fid_org:
+            for resolution in resolutions:
+                for chromosome in chromosomes:
+                    startingbp = 0
+                    endingbp = hic.getChromosomes()[int(chromosome)].length
+                    matrix_object = hic.getMatrixZoomData(chromosome, chromosome, data, "KR", "BP", resolution)
+                    matrix = matrix_object.getRecordsAsMatrix(startingbp, endingbp, startingbp, endingbp)
+                    for i in range(matrix.shape[0]):
+                        for j in range(i, matrix.shape[1]):
+                            if matrix[i, j] != 0:
+                                fid_org.write('0 {} {} 0 0 {} {} 1 {:.5f}\n'.format(chromosome, startingbp + resolution * i, chromosome, startingbp + resolution * j, matrix[i, j]))
+
+if __name__ == "__main__":
+    data_path = sys.argv[1]
+    hic_url = sys.argv[2]
+    resolutions_list = sys.argv[3]
+    chromosomes_list = sys.argv[4]
+    data_types_list = sys.argv[5]
+    extract_hic_data(data_path, hic_url, resolutions_list, chromosomes_list, data_types_list)
---
+++ utilities/getData.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# Description: The following script pulls hi-c matrices for further processing downstream
+
+# Set the path for data storage
+data_path=../projects/b1198/epifluidlab/david/GSE63525/GM12878/
+mkdir -p $data_path # mkdir if it doesn't exist
+
+# Variables
+resolutions=(100000)
+chromosomes=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22")
+data_types=("observed")
+genomeID="hg19"
+hic_url="https://ftp.ncbi.nlm.nih.gov/geo/series/GSE63nnn/GSE63525/suppl/GSE63525%5FGM12878%5Finsitu%5Fprimary%2Breplicate%5Fcombined%5F30%2Ehic"
+
+# Convert lists to strings for Python script
+resolutions_list=$(printf "'%s', " "${resolutions[@]}")
+chromosomes_list=$(printf "'%s', " "${chromosomes[@]}")
+data_types_list=$(printf "'%s', " "${data_types[@]}")
+
+resolutions_list=${resolutions_list%, }
+chromosomes_list=${chromosomes_list%, }
+data_types_list=${data_types_list%, }
+
+# Execute the Python script
+python extract_hic_data.py "$data_path" "$hic_url" "$resolutions_list" "$chromosomes_list" "$data_types_list"
+
+# Java command to create .hic files from short form text files
+for resolution in "${resolutions[@]}"; do
+    for chromosome in "${chromosomes[@]}"; do
+        for data in "${data_types[@]}"; do
+            path_to_jar="/projects/b1198/epifluidlab/david/softwareFiles/juicer_tools_1.22.01.jar"
+            path_to_short_form="${data_path}hicFiles/short_score_textform/shortScore_res${resolution}_ch${chromosome}_${data}_KR.txt"
+            path_to_new_hic="${data_path}hicFiles/individual/res${resolution}_ch${chromosome}_${data}_KR.hic"
+            java_command="java -Xmx100G -jar ${path_to_jar} pre -r ${resolution} ${path_to_short_form} ${path_to_new_hic} hg19"
+            echo "Running: $java_command"
+            $java_command
+        done
+    done
+done
+
+# Execute Pearson's correlation matrix
+for resolution in "${resolutions[@]}"; do
+    for chromosome in "${chromosomes[@]}"; do
+        path_to_hic="${data_path}hicFiles/individual/res${resolution}_ch${chromosome}_observed_KR.hic"
+        path_to_new_pearsons="${data_path}pearsons/individual/res${resolution}_ch${chromosome}_observed_KR_pearsons.txt"
+        pearsons_command="java -jar ${path_to_jar} pearsons -p NONE ${path_to_hic} ${chromosome} BP ${resolution} ${path_to_new_pearsons}"
+        echo "Running: $pearsons_command"
+        $pearsons_command
+    done
+done
+
+# Execute eigenvector calculation
+for resolution in "${resolutions[@]}"; do
+    for chromosome in "${chromosomes[@]}"; do
+        path_to_hic="${data_path}hicFiles/individual/res${resolution}_ch${chromosome}_observed_KR.hic"
+        path_to_new_eigenvector="${data_path}eigenvector/res${resolution}_ch${chromosome}_observed_KR_eigenvector.txt"
+        eigenvector_command="java -jar ${path_to_jar} eigenvector -p NONE ${path_to_hic} ${chromosome} BP ${resolution} ${path_to_new_eigenvector}"
+        echo "Running: $eigenvector_command"
+        $eigenvector_command
+    done
+done
+
+# Execute the second Python script
+python process_pearsons.py "$data_path" "$resolutions_list" "$chromosomes_list" "$data_types_list"

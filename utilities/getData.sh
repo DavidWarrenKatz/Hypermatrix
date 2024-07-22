59a4494aeeb6a8605338969f751759391f0528bf
David Warren Katz <59783036+DavidWarrenKatz@users.noreply.github.com>
2024-07-21 20:07:36 -0500

    Update getData.sh

--- utilities/getData.sh
+++ utilities/getData.sh
@@ -9,13 +9,13 @@
 
 
 # Set the path for data storage
-data_path=../projects/b1198/epifluidlab/david/GSE63525/GM12878/
+data_path=../projects/GSE63525/GM12878/
 mkdir -p $data_path # mkdir if it doesn't exist
 
 # Variables
-resolutions=(100000)
+resolutions=(1000000)
 chromosomes=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22")
-data_types=("observed")
+data_types=("oe")
 genomeID="hg19"
 hic_url="https://ftp.ncbi.nlm.nih.gov/geo/series/GSE63nnn/GSE63525/suppl/GSE63525%5FGM12878%5Finsitu%5Fprimary%2Breplicate%5Fcombined%5F30%2Ehic"
 
@@ -36,8 +36,12 @@ for resolution in "${resolutions[@]}"; do
     for chromosome in "${chromosomes[@]}"; do
         for data in "${data_types[@]}"; do
             path_to_jar="projects/softwarefiles/juicer_tools_1.22.01.jar"
-            path_to_short_form="${data_path}hicFiles/short_score_textform/shortScore_res${resolution}_ch${chromosome}_${data}_KR.txt"
-            path_to_new_hic="${data_path}hicFiles/individual/res${resolution}_ch${chromosome}_${data}_KR.hic"
+            short_form_directory="${data_path}hicFiles/short_score_textform/"
+            mkdir -p $short_form_directory # mkdir if it doesn't exist
+            path_to_short_form="$short_form_directory}shortScore_res${resolution}_ch${chromosome}_${data}_KR.txt"
+            hic_directory="${data_path}hicFiles/individual/"
+            mkdir -p $hic_directory # mkdir if it doesn't exist
+            path_to_new_hic="${hic_directory}res${resolution}_ch${chromosome}_${data}_KR.hic"
             java_command="java -Xmx100G -jar ${path_to_jar} pre -r ${resolution} ${path_to_short_form} ${path_to_new_hic} hg19"
             echo "Running: $java_command"
             $java_command
@@ -50,7 +54,9 @@ echo -e "[LOG]: Running Pearson's correlation matrix "
 for resolution in "${resolutions[@]}"; do
     for chromosome in "${chromosomes[@]}"; do
         path_to_hic="${data_path}hicFiles/individual/res${resolution}_ch${chromosome}_observed_KR.hic"
-        path_to_new_pearsons="${data_path}pearsons/individual/res${resolution}_ch${chromosome}_observed_KR_pearsons.txt"
+        pearson_directory="${data_path}pearsons/individual/"
+        mkdir -p $pearson_directory
+        path_to_new_pearsons="${pearson_directory}res${resolution}_ch${chromosome}_observed_KR_pearsons.txt"
         pearsons_command="java -jar ${path_to_jar} pearsons -p NONE ${path_to_hic} ${chromosome} BP ${resolution} ${path_to_new_pearsons}"
         echo "Running: $pearsons_command"
         $pearsons_command
@@ -61,7 +67,9 @@ done
 for resolution in "${resolutions[@]}"; do
     for chromosome in "${chromosomes[@]}"; do
         path_to_hic="${data_path}hicFiles/individual/res${resolution}_ch${chromosome}_observed_KR.hic"
-        path_to_new_eigenvector="${data_path}eigenvector/res${resolution}_ch${chromosome}_observed_KR_eigenvector.txt"
+        eigenvector_directory="${data_path}eigenvector/"
+        mkdir -p $eigenvector_directory
+        path_to_new_eigenvector="${eigenvector_directory}res${resolution}_ch${chromosome}_observed_KR_eigenvector.txt"
         eigenvector_command="java -jar ${path_to_jar} eigenvector -p NONE ${path_to_hic} ${chromosome} BP ${resolution} ${path_to_new_eigenvector}"
         echo "Running: $eigenvector_command"
         $eigenvector_command

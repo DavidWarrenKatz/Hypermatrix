42e583ec4febe369f734b57ce9eaca2eb18fbf18
David Warren Katz <59783036+DavidWarrenKatz@users.noreply.github.com>
2024-07-22 11:17:26 -0500

    Update getData.sh

--- utilities/getData.sh
+++ utilities/getData.sh
@@ -35,8 +35,7 @@ individual_workspace_directory="${data_path}Workspaces/individual/"
 mkdir -p $individual_workspace_directory # mkdir if it doesn't exist
 
 #download the dark regions file
-wget https://www.encodeproject.org/files/ENCFF000EHJ/@@download/ENCFF000EHJ.bigWig -O "../projects/softwarefiles/ENCFF000EHJ_hg19_wgEncodeCrgMapabilityAlign36mer.bigWig
-
+wget https://www.encodeproject.org/files/ENCFF000EHJ/@@download/ENCFF000EHJ.bigWig -O "../projects/softwarefiles/ENCFF000EHJ_hg19_wgEncodeCrgMapabilityAlign36mer.bigWig"
 
 # Execute the Python script
 python extract_hic_data.py "$data_path" "$hic_url" "$resolutions_list" "$chromosomes_list" "$data_types_list"

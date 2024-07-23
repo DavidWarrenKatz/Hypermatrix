7d9ff87458b177a3939b1227e41b2d54c7918035
David Warren Katz <59783036+DavidWarrenKatz@users.noreply.github.com>
2024-07-23 14:02:51 -0500

    Create config.py

---
+++ projects/softwarefiles/config.py
@@ -0,0 +1,8 @@
+# config.py
+
+path = '/home/dwk681/workspace/hypermatrix_test/hypermatrix/projects/GSE63525/GM12878/'
+resolutions = [1000000]
+chromosomes = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22']
+data_types = ['oe']
+bigwig_file = '/home/dwk681/workspace/hypermatrix_test/hypermatrix/projects/softwarefiles/ENCFF000EHJ_hg19_wgEncodeCrgMapabilityAlign36mer.bigWig'
+mappability_threshold = 0.5  

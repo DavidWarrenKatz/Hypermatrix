0162e559795dc92fe61f29f84b8a7a1fc50b2216
DavidWarrenKatz <David.Warren.Katz@gmail.com>
2024-07-30 20:50:36 -0500

    updated tensorlab zipping

--- utilities/runPipeline_bulkdata.sh
+++ utilities/runPipeline_bulkdata.sh
@@ -37,6 +37,39 @@ python extract_hic_data.py "$data_path" "$hic_url" "$resolutions_list" "$chromos
 
 python process_hic_files.py "$data_path" "$resolutions_list" "$chromosomes_list" "$data_types_list"
 
+#Check to make sure tensorlab is available
+# Define the paths
+zip_file="../projects/softwarefiles/tensorlab/tensorlab_2016-03-28.zip"
+unzip_dir="../projects/softwarefiles/tensorlab/"
+
+# Check if the zip file exists
+if [ ! -f "$zip_file" ]; then
+  echo "Zip file does not exist."
+  exit 1
+fi
+
+# Get the list of files in the zip archive
+zip_files=$(unzip -l "$zip_file" | awk 'NR>3 {print $4}' | sed '$d')
+
+# Function to check if all files from the zip are already in the directory
+all_files_exist() {
+  for file in $zip_files; do
+    if [ ! -f "$unzip_dir/$file" ]; then
+      return 1
+    fi
+  done
+  return 0
+}
+
+# Check if all files exist
+if all_files_exist; then
+  echo "All files from the zip are already present in the directory. Skipping unzip."
+else
+  echo "Unzipping the file..."
+  unzip "$zip_file" -d "$unzip_dir"
+  echo "Unzipping completed."
+fi
+
 # Load the necessary modules
 module load matlab/r2022b
 

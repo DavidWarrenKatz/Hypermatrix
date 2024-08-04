b5ea4c5899cce587e7445f39a330c6a0d631a209
DavidWarrenKatz <David.Warren.Katz@gmail.com>
2024-08-04 09:19:52 -0500

    changes

--- utilities/getStructuredData.m
+++ utilities/getStructuredData.m
@@ -6,12 +6,13 @@ config = load('config.mat');
 data_path = config.data_path;
 resolutions = config.resolutions;
 chromosomes = cellstr(config.chromosomes); % Convert to cell array of character vectors if needed
+iterations = config.iterations;
 
 for resolution = resolutions
     for j = 1:numel(chromosomes)
         chromosome = chromosomes{j};
         file_path = sprintf('%sWorkspaces/individual/ch%s_res%d_oe_KR_cumulant.h5', data_path, chromosome, resolution);
-        output_file = sprintf('%sWorkspaces/individual/ch%s_res%d_structuredData_3rdCumulant_rank10_%diterations.h5', data_path, chromosome, resolution, iterations);
+        output_file = sprintf('%sWorkspaces/individual/ch%s_res%d_structuredData_3rdCumulant_rank2_%diterations.h5', data_path, chromosome, resolution, iterations);
         
         % Check if the output file already exists
         if exist(output_file, 'file') == 2
@@ -22,7 +23,7 @@ for resolution = resolutions
         dataset_name = '/degree_3_cumulant';
         cumulant = h5read(file_path, dataset_name);
         model = struct;
-        model.variables.u = randn(size(cumulant, 1), 3);
+        model.variables.u = randn(size(cumulant, 1), 2);
         model.factors.U = {'u', @struct_nonneg};
         model.factorizations.myfac.data = cumulant;
         model.factorizations.myfac.cpd = {'U', 'U', 'U'};

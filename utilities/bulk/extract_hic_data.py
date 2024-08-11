16b0bd2bbc6a78e97524ef619596830b2db58d70
chewe.kasonde@outlook.com <chewe.kasonde@outlook.com>
2024-08-11 13:23:28 -0400

    removed .mat to .h5 redundancy

--- utilities/bulk/extract_hic_data.py
+++ utilities/bulk/extract_hic_data.py
@@ -18,18 +18,27 @@ def extract_hic_data(path, hic_url, resolutions, chromosomes, data_types):
             for data in data_types:
                 matrix_object = hic.getMatrixZoomData(chromosome, chromosome, data, normalization, "BP", resolution)
                 matrix = matrix_object.getRecordsAsMatrix(startingbp, endingbp, startingbp, endingbp)
-                with open(f'{path}hicFiles/short_score_textform/shortScore_res{resolution}_ch{chromosome}_{data}_{normalization}.txt', 'wt') as fid_org:
+                
+                # Save short score to text file
+                short_score_path = f'{path}hicFiles/short_score_textform/shortScore_res{resolution}_ch{chromosome}_{data}_{normalization}.txt'
+                with open(short_score_path, 'wt') as fid_org:
                     for i in range(matrix.shape[0]):
                         for j in range(i, matrix.shape[1]):
                             if matrix[i, j] != 0:
-                                fid_org.write('0 {} {} 0 0 {} {} 1 {:.5f}\n'.format(chromosome, startingbp + resolution * i, chromosome, startingbp + resolution * j, matrix[i, j]))
+                                fid_org.write('0 {} {} 0 0 {} {} 1 {:.5f}\n'.format(
+                                    chromosome, startingbp + resolution * i, chromosome, startingbp + resolution * j, matrix[i, j]))
+                print(f"[DEBUG] Saved short score file to: {short_score_path}")
 
-                new_filename = f'{path}Workspaces/individual/ch{chromosome}_res{resolution}_{data}_{normalization}.mat'
-                with h5py.File(new_filename, 'w') as hf:
+                # Save matrix directly as .h5 file
+                h5_filename = f'{path}Workspaces/individual/ch{chromosome}_res{resolution}_{data}_{normalization}.h5'
+                with h5py.File(h5_filename, 'w') as hf:
                     hf.create_dataset('matrix', data=matrix)
+                print(f"[DEBUG] Saved .h5 file to: {h5_filename}")
 
+    # Generate combined short score files for all chromosomes
     for data in data_types:
-        with open(f'{path}hicFiles/short_score_textform/shortScore_all_{data}_KR.txt', 'wt') as fid_org:
+        combined_short_score_path = f'{path}hicFiles/short_score_textform/shortScore_all_{data}_KR.txt'
+        with open(combined_short_score_path, 'wt') as fid_org:
             for resolution in resolutions:
                 for chromosome in chromosomes:
                     startingbp = 0
@@ -39,7 +48,9 @@ def extract_hic_data(path, hic_url, resolutions, chromosomes, data_types):
                     for i in range(matrix.shape[0]):
                         for j in range(i, matrix.shape[1]):
                             if matrix[i, j] != 0:
-                                fid_org.write('0 {} {} 0 0 {} {} 1 {:.5f}\n'.format(chromosome, startingbp + resolution * i, chromosome, startingbp + resolution * j, matrix[i, j]))
+                                fid_org.write('0 {} {} 0 0 {} {} 1 {:.5f}\n'.format(
+                                    chromosome, startingbp + resolution * i, chromosome, startingbp + resolution * j, matrix[i, j]))
+        print(f"[DEBUG] Saved combined short score file to: {combined_short_score_path}")
 
 if __name__ == "__main__":
     data_path = sys.argv[1]
@@ -47,10 +58,11 @@ if __name__ == "__main__":
     resolutions_list = sys.argv[3]
     chromosomes_list = sys.argv[4]
     data_types_list = sys.argv[5]
+
     print(f"[DEBUG] Input data_path: {data_path}")
     print(f"[DEBUG] Input hic_url: {hic_url}")
     print(f"[DEBUG] Input resolutions_list: {resolutions_list}")
     print(f"[DEBUG] Input chromosomes_list: {chromosomes_list}")
     print(f"[DEBUG] Input data_types_list: {data_types_list}")
-    extract_hic_data(data_path, hic_url, resolutions_list, chromosomes_list, data_types_list)
 
+    extract_hic_data(data_path, hic_url, resolutions_list, chromosomes_list, data_types_list)
